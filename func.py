6a9495825bc9cb3cf14fed546b551642f6862528
pavlig43 <pavlig43@gmail.com>
2023-06-11 23:38:35 +0300

    пароль глаз

---
+++ func.py
@@ -0,0 +1,19 @@
+import json
+
+
+def get_list_value(file_name):
+    try:
+        with open(file_name, 'r') as file:
+            try:
+                value = json.load(file)
+            except json.decoder.JSONDecodeError:
+                value = {}
+    except FileNotFoundError:
+        with open(file_name, 'w') as file:
+            value = {}
+    return value
+
+
+if __name__ == '__main__':
+    a = get_list_value('list.json')
+    print(a.keys())
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from kivy.lang import Builder
-from kivy.properties import StringProperty, ListProperty,BooleanProperty, ObjectProperty
+from kivy.properties import StringProperty, ListProperty, BooleanProperty, ObjectProperty
 from kivy.clock import Clock
 from kivymd.app import MDApp
 from kivymd.theming import ThemableBehavior
@@ -10,12 +10,18 @@ from kivymd.uix.screen import MDScreen
 from kivy.properties import NumericProperty
 from kivy.metrics import dp
 from kivymd.uix.textfield import MDTextField
+from kivymd.uix.card import MDCard
 from functools import partial
+from kivymd.uix.dialog import MDDialog
+import json
+from func import get_list_value
+
 
 
 
 class LoginWindow(MDScreen):
     pass
+
 
 class PasswordMDTextField(MDTextField):
     password_mode = BooleanProperty(True)
@@ -44,13 +50,12 @@ class PasswordMDTextField(MDTextField):
 
                     # try to adjust cursor position
                     cursor = self.cursor
-                    self.cursor = (0,0)
+                    self.cursor = (0, 0)
                     Clock.schedule_once(partial(self.set_cursor, cursor))
         return super(PasswordMDTextField, self).on_touch_down(touch)
 
     def set_cursor(self, pos, dt):
         self.cursor = pos
-
 
 
 class ContentNavigationDrawer(MDBoxLayout):
@@ -60,15 +65,35 @@ class ContentNavigationDrawer(MDBoxLayout):
 class ItemDrawer(OneLineIconListItem):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
+
     icon = StringProperty()
     text_color = ListProperty((0, 0, 0, 1))
-    function = ObjectProperty()
+    screen_name = StringProperty()
 
 
 class MainMenu(MDScreen):
     pass
 
-class Lesson(MDScreen):
+
+
+class Profile(MDScreen):
+    pass
+
+
+class Modules(MDScreen):
+    def add_folder(self):
+        dialog = WindowAddFolder()
+        dialog.open()
+
+class FolderList(MDList):
+    def __init__(self,folders=[], **kwargs):
+        super().__init__(**kwargs)
+        self.folders = list(get_list_value('folders.json'))
+        for folder in self.folders:
+            item = ItemDrawer(icon= 'folder', text = folder)
+            self.add_widget(item)
+
+class WindowAddFolder(MDDialog):
     pass
 
 
@@ -76,43 +101,29 @@ class DrawerList(MDList):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         icons_item = {
-            "account-box": {'text':"Профиль", 'function':self.profile},
-            "folder-multiple-outline": {'text':"Мои модули",'function': None},
-            "school": {'text':"Тестирование",'function': None},
-            "history": {'text':"Статистика",'function': None},
-            'file-cog': {'text': 'Настройки','function': None},
-            r"C:\Users\user\PycharmProjects\dict_for_phone\telegram.ico": {'text':"Связь с разработчиком",'function': None},
+            "account-box": {'text': "Профиль",  'screen_name': 'profile'},
+            "folder-multiple-outline": {'text': "Мои модули", 'screen_name': 'modules'},
+            "school": {'text': "Тестирование", 'screen_name': ''},
+            "history": {'text': "Статистика", 'screen_name': ''},
+            'file-cog': {'text': 'Настройки', 'screen_name': ''},
+            r"C:\Users\user\PycharmProjects\dict_for_phone\telegram.ico": {'text': "Связь с разработчиком",
+                                                                           'screen_name': ''},
         }
 
-
-        for icon_name, icon_info  in icons_item.items():
-            item = ItemDrawer(icon=icon_name, text=icon_info['text'], function=icon_info['function'])
-            item.bind(on_release= self.menu_callback)
+        for icon_name, icon_info in icons_item.items():
+            item = ItemDrawer(icon=icon_name, text=icon_info['text'],screen_name = icon_info['screen_name'] )
+            item.bind(on_release=self.menu_callback)
             self.add_widget(item)
 
-    def profile(self):
-        MDApp.get_running_app().root.current = 'lesson'
-        # a = MDApp.get_running_app()
-        # c = a.root.get_screen('loginwindow')
-        # print(a.root.screens)
-        # print(c)
-
-        # for i in dir(a):
-        #     if not i.startswith('_'):
-        #         print(i)
-
-
-
-
     def menu_callback(self, instance):
-        if instance.function:
-            instance.function()
+        try:
+            app = MDApp.get_running_app()
+            app.root.children[0].ids['nav_drawer'].set_state('close')
+            main_screen_manager = app.root.children[0].ids['mainscreenmanager']
+            main_screen_manager.current = instance.screen_name
 
-
-
-
-
-
+        except:
+            pass
 
     def set_color_item(self, instance_item):
         """Called when tap on a menu item."""
@@ -129,8 +140,6 @@ class Dictionari(MDApp):
     def build(self):
         self.theme_cls.primary_palette = "Pink"
         return Builder.load_file('dictionary.kv')
-
-
 
 
 if __name__ == "__main__":
