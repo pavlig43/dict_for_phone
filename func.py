76a3c9f49983325b3c7112b0e75a9085fc544598
pavlig43 <pavlig43@gmail.com>
2023-06-26 00:08:24 +0300

    созданы папки

--- func.py
+++ func.py
@@ -25,5 +25,37 @@ def tg_message(message):
     requests.get(url)
 
 
+def a():
+    from kivy.lang import Builder
+    from kivy.properties import StringProperty
+
+    from kivymd.app import MDApp
+    from kivymd.uix.card import MDCardSwipe
+
+    class AX(MDCardSwipe):
+        text = StringProperty()
+
+    class Example(MDApp):
+        def __init__(self, **kwargs):
+            super().__init__(**kwargs)
+            self.theme_cls.theme_style = "Dark"
+            self.theme_cls.primary_palette = "Orange"
+            self.screen = Builder.load_file('ax.kv')
+
+        def build(self):
+            return self.screen
+
+        def remove_item(self, instance):
+            self.screen.ids.md_list.remove_widget(instance)
+
+        def on_start(self):
+            for i in range(20):
+                self.screen.ids.md_list.add_widget(
+                    AX(text=f"One-line item {i}")
+                )
+
+    Example().run()
+
+
 if __name__ == '__main__':
-    tg_message('Hello')
+    a()
--- main.py
+++ main.py
@@ -4,24 +4,22 @@ from kivy.clock import Clock
 from kivymd.app import MDApp
 from kivymd.theming import ThemableBehavior
 from kivymd.uix.boxlayout import MDBoxLayout
-from kivymd.uix.list import OneLineIconListItem, MDList
+from kivymd.uix.list import OneLineIconListItem, MDList, OneLineAvatarIconListItem
 from kivy.uix.screenmanager import ScreenManager
 from kivymd.uix.screen import MDScreen
 from kivymd.uix.button import MDFlatButton
 from kivy.properties import NumericProperty
 from kivy.metrics import dp
 from kivymd.uix.textfield import MDTextField
-from kivymd.uix.card import MDCard
+from kivymd.uix.card import MDCard, MDCardSwipe
 from functools import partial
 from kivymd.uix.dialog import MDDialog
+from kivymd.uix.label import MDLabel
 import json
 from func import get_list_value, tg_message
 import requests
 from kivymd.uix.pickers import MDColorPicker
 from kivymd.theming import ThemeManager
-
-
-
 
 
 class LoginWindow(MDScreen):
@@ -51,7 +49,6 @@ class PasswordMDTextField(MDTextField):
                     else:
                         self.icon_right = 'eye-off'
                         self.password_mode = True
-                        self.hint_text = hint_text
                         self.password = self.password_mode
 
                     # try to adjust cursor position
@@ -68,10 +65,11 @@ class ContentNavigationDrawer(MDBoxLayout):
     pass
 
 
-class ItemDrawer(OneLineIconListItem):
+class ItemDrawer(OneLineAvatarIconListItem):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
+    icon_right = StringProperty()
     icon = StringProperty()
     text_color = ListProperty((0, 0, 0, 1))
     screen_name = StringProperty()
@@ -90,14 +88,13 @@ class ItemDrawer(OneLineIconListItem):
 class MainMenu(MDScreen):
     pass
 
+
 class Setting(MDScreen):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        self.color_picker = MDColorPicker(size_hint=(0.45, 0.85), text_button_cancel = "C")
-
+        self.color_picker = MDColorPicker(size_hint=(0.45, 0.85), text_button_cancel="C")
 
     def open_pick(self):
-
         self.color_picker.open()
 
 
@@ -115,25 +112,42 @@ class Modules(MDScreen):
                                           MDFlatButton(
                                               text="Добавить",
                                               theme_text_color="Custom",
-                                              #text_color=self.theme_cls.primary_color,
+                                              # text_color=self.theme_cls.primary_color,
                                               on_release=self.add_folder
                                           ),
                                           MDFlatButton(
                                               text="Отмена",
                                               theme_text_color="Custom",
-                                              #text_color=self.theme_cls.primary_color,
+                                              # text_color=self.theme_cls.primary_color,
                                               on_release=lambda x: self.dialog_folder.dismiss()
                                           ),
                                       ],
                                       )
+        self.message_warning = MDDialog(type="custom",
+                                        content_cls=MDLabel(text=f'Удалить?'),
+                                        buttons=[
+                                            MDFlatButton(
+                                                text="Да",
+                                                theme_text_color="Custom",
+                                                # text_color=self.theme_cls.primary_color,
+
+                                            ),
+                                            MDFlatButton(
+                                                text="Отмена",
+                                                theme_text_color="Custom",
+                                                # text_color=self.theme_cls.primary_color,
+                                                on_release=lambda x: self.message_warning.dismiss()
+                                            ),
+                                        ],
+                                        )
 
     def open_dialog(self, *args):
         self.dialog_folder.open()
 
     def add_folder(self, *args):
         folder_name = self.dialog_folder.content_cls.ids.folder_dialog_text.text
-        folder_dict = self.ids.folder_list.folders
-        if folder_name in folder_dict:
+        self.folder_dict = self.ids.folder_list.folders
+        if folder_name in self.folder_dict:
             message_error = MDDialog(text='Папка с таким именем уже существует',
                                      buttons=[MDFlatButton(
                                          text='ОК',
@@ -141,16 +155,27 @@ class Modules(MDScreen):
                                      )])
             message_error.open()
         else:
-            folder_dict[folder_name] = {}
-            item = ItemDrawer(icon='folder', text=folder_name, screen_name=folder_name)
+            self.folder_dict[folder_name] = {}
+            item = ItemDrawer(icon='folder', text=folder_name, screen_name=folder_name, icon_right='delete')
             with open('folders.json', 'w') as f:
-                json.dump(folder_dict, f)
+                json.dump(self.folder_dict, f)
             self.ids.folder_list.add_widget(item)
             item.bind(on_release=self.ids.folder_list.to_eximine)
+            item.ids.icon_right.bind(on_release=lambda x, item=item: self.open_warning_dialog(item))
             self.dialog_folder.content_cls.ids.folder_dialog_text.text = ''
             self.dialog_folder.dismiss()
 
+    def open_warning_dialog(self, folder):
+        self.message_warning.open()
+        self.message_warning.buttons[0].bind(
+            on_release=lambda x: (self.delete_folder(folder), self.message_warning.dismiss()))
 
+    def delete_folder(self, folder):
+
+        del self.folder_dict[folder.text]
+        with open('folders.json', 'w') as f:
+            json.dump(self.folder_dict, f)
+        self.ids.folder_list.remove_widget(folder)
 
 
 class FolderList(MDList):
@@ -158,9 +183,21 @@ class FolderList(MDList):
         super().__init__(**kwargs)
         self.folders = get_list_value('folders.json')
         for folder in list(self.folders):
-            item = ItemDrawer(icon='folder', text=folder, screen_name=folder)
+            item = ItemDrawer(icon='folder', text=folder, screen_name=folder, icon_right='delete')
+            item.ids.icon_right.bind(on_release=lambda x, item=item: self.open_warning_dialog(item))
             item.bind(on_release=self.to_eximine)
             self.add_widget(item)
+
+    def open_warning_dialog(self, folder):
+        message_warning = self.parent.parent.parent.parent.parent.message_warning
+        message_warning.open()
+        message_warning.buttons[0].bind(
+            on_release=lambda x: (self.delete_folder(folder), message_warning.dismiss()))
+    def delete_folder(self, folder):
+        self.remove_widget(folder)
+        del self.folders[folder.text]
+        with open('folders.json', 'w') as f:
+            json.dump(self.folders, f)
 
     def to_eximine(self, instance):
         sm = self.parent.parent.parent.parent
@@ -172,48 +209,54 @@ class FolderList(MDList):
             eximine_screen = Eximine(name=instance.screen_name, name_folder=instance.screen_name)
             sm.add_widget(eximine_screen)
 
-
         sm.current = instance.screen_name
 
 
 class EximineList(MDList):
-    def __init__(self, modules=[],name_folder='', **kwargs):
+    def __init__(self, modules=[], name_folder='', **kwargs):
         super().__init__(**kwargs)
         self.name_folder = name_folder
         self.modules = get_list_value('folders.json')[self.name_folder]
 
         for module in self.modules:
-            item = ItemDrawer(icon="school", text=module, screen_name=module)
+            item = ItemDrawer(icon="school", text=module, screen_name=module, icon_right='delete')
             item.bind(on_release=self.open_module)
+            item.ids.icon_right.bind(on_release=lambda x, item=item: self.open_warning_dialog(item))
             self.add_widget(item)
+    def open_warning_dialog(self, module):
+        message_warning = self.parent.parent.parent.parent.parent.message_warning
+        message_warning.open()
+        message_warning.buttons[0].bind(
+            on_release=lambda x: (self.delete_folder(module), message_warning.dismiss()))
+    def delete_folder(self, module):
+        self.remove_widget(module)
+        with open('folders.json', 'r') as f:
+            dct = json.load(f)
+            del dct[self.name_folder][module.text]
+        with open('folders.json', 'w') as f:
+            json.dump(dct, f)
 
     def open_module(self, instance):
-
 
         sm = self.parent.parent.parent.parent
         folder = self.parent.parent.parent.parent.parent.name
         module = instance.text
-        print(folder, module)
+
         try:
             sm.get_screen(instance.text)
         except:
 
-            words_screen = Cards(name=instance.text, folder =folder, module=module)
-            sm.add_widget(words_screen)
-
+            cards = Cards(name=instance.text, folder=folder, module=module)
+            sm.add_widget(cards)
 
         sm.current = instance.screen_name
 
 
-
-
-
 class DialogFolder(MDBoxLayout):
-    def __init__(self,hint_text = '',**kwargs):
+    def __init__(self, hint_text='', **kwargs):
         super().__init__(**kwargs)
         self.hint_text = hint_text
         self.ids.folder_dialog_text.hint_text = self.hint_text
-
 
 
 class Eximine(MDScreen):
@@ -228,20 +271,38 @@ class Eximine(MDScreen):
                                           MDFlatButton(
                                               text="Добавить",
                                               theme_text_color="Custom",
-                                              #text_color=self.theme_cls.primary_color,
-                                              on_release= self.add_module
+                                              # text_color=self.theme_cls.primary_color,
+                                              on_release=self.add_module
                                           ),
                                           MDFlatButton(
                                               text="Отмена",
                                               theme_text_color="Custom",
-                                              #text_color=self.theme_cls.primary_color,
+                                              # text_color=self.theme_cls.primary_color,
                                               on_release=lambda x: self.dialog_folder.dismiss()
                                           ),
                                       ],
                                       )
+        self.message_warning = MDDialog(type="custom",
+                                        content_cls=MDLabel(text=f'Удалить?'),
+                                        buttons=[
+                                            MDFlatButton(
+                                                text="Да",
+                                                theme_text_color="Custom",
+                                                # text_color=self.theme_cls.primary_color,
+
+                                            ),
+                                            MDFlatButton(
+                                                text="Отмена",
+                                                theme_text_color="Custom",
+                                                # text_color=self.theme_cls.primary_color,
+                                                on_release=lambda x: self.message_warning.dismiss()
+                                            ),
+                                        ],
+                                        )
 
     def open_dialog(self, *args):
         self.dialog_folder.open()
+
     def add_module(self, *args):
         module_name = self.dialog_folder.content_cls.ids.folder_dialog_text.text
         module_dict = self.eximine_list.modules
@@ -255,8 +316,9 @@ class Eximine(MDScreen):
 
         else:
             module_dict[module_name] = {}
-            item = ItemDrawer(icon='school', text=module_name, screen_name=module_name)
+            item = ItemDrawer(icon='school', text=module_name, screen_name=module_name, icon_right='delete')
             item.bind(on_release=self.eximine_list.open_module)
+            item.ids.icon_right.bind(on_release=lambda x, item=item: self.open_warning_dialog(item))
             self.eximine_list.add_widget(item)
             with open('folders.json', 'r') as f:
                 dct = json.load(f)
@@ -264,14 +326,83 @@ class Eximine(MDScreen):
             with open('folders.json', 'w') as f:
                 json.dump(dct, f)
             self.dialog_folder.dismiss()
+        self.dialog_folder.content_cls.ids.folder_dialog_text.text = ''
+    def open_warning_dialog(self, module):
+        self.message_warning.open()
+        self.message_warning.buttons[0].bind(
+            on_release=lambda x: (self.delete_folder(module), self.message_warning.dismiss()))
+
+    def delete_folder(self, module):
+        self.eximine_list.remove_widget(module)
+
+        with open('folders.json', 'r') as f:
+            dct = json.load(f)
+            del dct[self.name_folder][module.text]
+        with open('folders.json', 'w') as f:
+            json.dump(dct, f)
 
 class Cards(MDScreen):
-    def __init__(self,folder ='',module='',**kwargs):
+    def __init__(self, folder='', module='', **kwargs):
         super().__init__(**kwargs)
         self.folder = folder
         self.module = module
         self.ids.name_module.text = f'{self.folder} \n{self.module}'
 
+    def change_module_screen(self):
+        sm = self.parent
+        try:
+            sm.get_screen('change_module')
+        except:
+            self.change_module = ChangeModule(name='change_module', folder=self.folder, module=self.module)
+            sm.add_widget(self.change_module)
+        self.words = get_list_value('folders.json')
+        for word, definition in self.words[self.folder][self.module].items():
+            word_def = WordDef(word=word, definition=definition)
+            self.change_module.ids.boxlayout_words.add_widget(word_def)
+            word_def.ids.word.text = word
+            word_def.ids.definition.text = definition
+
+        sm.current = 'change_module'
+
+
+class ChangeModule(MDScreen):
+    def __init__(self, folder='', module='', **kwargs):
+        super().__init__(**kwargs)
+
+        self.words = {}
+        self.folder = folder
+        self.module = module
+        self.ids.name_field.text = self.module
+
+    def add_word(self):
+        self.ids.boxlayout_words.add_widget(WordDef())
+        self.ids.scroll_word.scroll_to(self.ids.boxlayout_words.children[0])
+
+    def save_data(self):
+
+        self.words = get_list_value('folders.json')
+        self.all_words_dict = {}
+        for i in self.ids.boxlayout_words.children:
+            if i.ids.word.text in self.all_words_dict:
+                i.ids.word.text += ' '
+            self.all_words_dict[i.ids.word.text] = i.ids.definition.text
+
+        if not self.ids.name_field.text == self.module:
+            del self.words[self.folder][self.module]
+            self.module = self.ids.name_field.text
+        self.words[self.folder][self.module] = self.all_words_dict
+        with open('folders.json', 'w') as f:
+            json.dump(self.words, f)
+        self.parent.current = self.parent.previous()
+        self.parent.remove_widget(self)
+
+
+class WordDef(MDBoxLayout):
+    def __init__(self, word='', definition='', **kwargs):
+        super().__init__(**kwargs)
+
+        self.word = word
+        self.definition = definition
 
 
 class DrawerList(MDList):
@@ -281,7 +412,7 @@ class DrawerList(MDList):
             "account-box": {'text': "Профиль", 'screen_name': 'profile'},
             "folder-multiple-outline": {'text': "Мои модули", 'screen_name': 'modules'},
             "school": {'text': "Тестирование", 'screen_name': ''},
-            "history": {'text': "Статистика", 'screen_name': ''},
+            "history": {'text': "Статистика", 'screen_name': 'history'},
             'file-cog': {'text': 'Настройки', 'screen_name': 'setting'},
             r"C:\Users\user\PycharmProjects\dict_for_phone\telegram.ico": {'text': "Связь с разработчиком",
                                                                            'screen_name': 'contact_developer'},
@@ -315,7 +446,8 @@ class ContactDeveloper(MDScreen):
 
 
 class Dictionari(MDApp):
-    theme_cls =ThemeManager()
+    theme_cls = ThemeManager()
+
     def build(self):
         self.theme_cls.primary_palette = "Pink"
 
