518ab31f1c1b969e8a089b011f8650a097d328f9
pavlig43 <pavlig43@gmail.com>
2023-07-03 20:43:42 +0300

    созданы папки

--- func.py
+++ func.py
@@ -1,6 +1,7 @@
 import json
 
 import requests
+from googletrans import Translator
 
 
 def get_list_value(file_name):
@@ -16,46 +17,38 @@ def get_list_value(file_name):
     return value
 
 
-token = '6118490871:AAGTVlmAw3-a12tkEy8N9W6gXl2ofmD_8FU'
-chat_id = '5274223669'
-
-
 def tg_message(message):
+    token = '6118490871:AAGTVlmAw3-a12tkEy8N9W6gXl2ofmD_8FU'
+    chat_id = '5274223669'
     url = rf'https://api.telegram.org/bot{token}/sendMessage?chat_id={chat_id}&text={message}'
     requests.get(url)
 
 
-def a():
-    from kivy.lang import Builder
-    from kivy.properties import StringProperty
+def translate_word(word):
+    translator = Translator()
+    try:
+        result = translator.translate(word, dest='ru')
+        return result.text
+    except:
+        return word
 
-    from kivymd.app import MDApp
-    from kivymd.uix.card import MDCardSwipe
 
-    class AX(MDCardSwipe):
-        text = StringProperty()
+def dictionary_txt():
+    dictionary = {}
+    with open('dictionary.txt', 'r', encoding='utf-8') as file:
+        all_text = file.read().split('\n')[:10]
+        for string in all_text:
+            string = string.replace('"','').split(';')
+            dictionary[string[0]] = {'translation': string[-1].split(","), 'pronunciations': string[-2] if
+                                     len(string) == 3 else ''}
+            # string = string.replace('"', '').rsplit(';',1)
+            # term_transcription = string[0].replace(';', ' ')
+            # term_transcription = string[0].split(';')[0]
+            # values = string[1].split(',')
+            # dictionary[term_transcription] = values
 
-    class Example(MDApp):
-        def __init__(self, **kwargs):
-            super().__init__(**kwargs)
-            self.theme_cls.theme_style = "Dark"
-            self.theme_cls.primary_palette = "Orange"
-            self.screen = Builder.load_file('ax.kv')
-
-        def build(self):
-            return self.screen
-
-        def remove_item(self, instance):
-            self.screen.ids.md_list.remove_widget(instance)
-
-        def on_start(self):
-            for i in range(20):
-                self.screen.ids.md_list.add_widget(
-                    AX(text=f"One-line item {i}")
-                )
-
-    Example().run()
+        return dictionary
 
 
 if __name__ == '__main__':
-    a()
+    print(dictionary_txt())
